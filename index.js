b484914e1997ea87413be62e4cf4734731ee2021
aclementcl <alejandro.clemente.i@gmail.com>
2022-04-20 16:50:46 -0400

    Se crean endpoints con SRP usando router

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hola server');
+  res.send('Inicio');
 });
+
+routerApi(app);
 
 app.listen(port, () => {
   console.log(`El puerto es: ${port}`);
